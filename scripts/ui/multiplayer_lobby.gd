b164b0559cade9ed9b373e7af642a882a468a83a
Conjunto-de-particulas <camilo.coc1228@gmail.com>
2026-02-25 15:43:56 -0500

    Add Lobby v.2

--- scripts/ui/multiplayer_lobby.gd
+++ scripts/ui/multiplayer_lobby.gd
@@ -1,12 +1,54 @@
 extends Control
 
+@onready var main_container : VBoxContainer = $VBoxContainer
 @onready var host_button: Button = $VBoxContainer/Host
 @onready var join_button: Button = $VBoxContainer/Join
+@onready var ip_line: LineEdit = $VBoxContainer/IPAddress
 
+@onready var scroll_container: ScrollContainer = $ScrollContainer
+@onready var players_container: VBoxContainer = $ScrollContainer/PlayersContainer
 
+@onready var start_button: Button = $StartButton
+
+func _ready() -> void:
+	ServerManager.player_connected.connect(_on_player_list_changed)
+	ServerManager.server_disconnected.connect(_on_server_disconnected)
+	
+	scroll_container.hide()
+	start_button.hide()
+	
 func _on_host_pressed() -> void:
-	ServerManager.create_game()
-
-
+	var error = ServerManager.create_game()
+	if error:
+		return
+		
+	main_container.hide()
+	scroll_container.show()
+	start_button.show()
+	
 func _on_join_pressed() -> void:
-	ServerManager.join_game()
+	var error = ServerManager.join_game(ip_line.text)
+	if error:
+		return
+	
+	main_container.hide()
+	scroll_container.show()
+
+func _on_player_list_changed(_id, _info) -> void:
+	for child in players_container.get_children():
+		child.queue_free()
+	
+	for p_id in ServerManager.players:
+		var info = ServerManager.players[p_id]
+		var label = Label.new()
+		label.text = info["name"]
+		players_container.add_child(label)
+		
+func _on_server_disconnected():
+	for child in players_container.get_children():
+		child.queue_free()
+		
+	main_container.show()
+	scroll_container.hide()
+	start_button.hide()
+	
